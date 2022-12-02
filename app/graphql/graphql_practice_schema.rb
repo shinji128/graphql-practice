2950cf9e99c59fbdba76cd819371e92b0fc16d5a
miura <s.miura@mof-mof.co.jp>
2022-12-02 15:21:43 +0900

    graphql-batchの設定追加

--- app/graphql/graphql_practice_schema.rb
+++ app/graphql/graphql_practice_schema.rb
@@ -1,6 +1,7 @@
 class GraphqlPracticeSchema < GraphQL::Schema
   mutation(Types::MutationType)
   query(Types::QueryType)
+  use GraphQL::Batch
 
   # For batch-loading (see https://graphql-ruby.org/dataloader/overview.html)
   use GraphQL::Dataloader
