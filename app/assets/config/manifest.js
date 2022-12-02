167700b931330fc2e89fd05d8d4b0c29391f830a
miura <s.miura@mof-mof.co.jp>
2022-12-02 15:19:22 +0900

    manifest.js修正

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,6 @@
 //= link_tree ../images
 //= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
 //= link_tree ../builds
+
+//= link graphiql/rails/application.css
+//= link graphiql/rails/application.js
