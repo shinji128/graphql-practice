9dc39c5279c91ab28446ddcfc97f89bbc31c7bc1
miura <s.miura@mof-mof.co.jp>
2022-12-02 12:14:15 +0900

    テストデータ追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+3.times do |i|
+  User.create(name: "ユーザー#{i}")
+end
+
+3.times do |i|
+  Post.create(title: "タイトル#{i}", user_id: 1)
+end
+
+3.times do |i|
+  Post.create(title: "タイトル#{i}", user_id: 2)
+end
+
+Post.create(title: 'タイトル1', user_id: 3)
