531bc9d6749971c4a61a4bdcb5ec074fceb471e6
miura <s.miura@mof-mof.co.jp>
2022-12-02 15:18:03 +0900

    テストデータ修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,12 +2,8 @@
   User.create(name: "ユーザー#{i}")
 end
 
-3.times do |i|
+2.times do |i|
   Post.create(title: "タイトル#{i}", user_id: 1)
 end
 
-3.times do |i|
-  Post.create(title: "タイトル#{i}", user_id: 2)
-end
-
-Post.create(title: 'タイトル1', user_id: 3)
+Post.create(title: 'タイトル1', user_id: 2)
